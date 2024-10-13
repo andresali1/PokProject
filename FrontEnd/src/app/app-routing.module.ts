2e716935798facbc9c92fc7d95c4a70d6b27dc5b
Andrés Salinas <andresali1@hotmail.es>
2024-10-13 11:08:13 +0200

    Estructura inicial del proyecto

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { SecurityComponent } from './modules/security/security.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    loadChildren: () =>
+      import('./modules/home/home.routing.module').then(
+        (m) => m.HomeRoutingModule
+      ),
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    loadChildren: () =>
+      import('./modules/home/home.routing.module').then(
+        (m) => m.HomeRoutingModule
+      ),
+  },
+  {
+    path: 'auth',
+    component: SecurityComponent,
+    loadChildren: () =>
+      import('./modules/security/security.routing.module').then(
+        (m) => m.SecurityRoutingModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material/material.module';
+import { HomeComponent } from './modules/home/home.component';
+import { SecurityComponent } from './modules/security/security.component';
+import { HomeModule } from './modules/home/home.module';
+import { SecurityModule } from './modules/security/security.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, SecurityComponent],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HomeModule,
+    MaterialModule,
+    SecurityModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
